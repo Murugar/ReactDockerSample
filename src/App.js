c40b4128f113209a1b6afa4be6d473c3a127a830
Davanon <davanon2014@gmail.com>
2017-12-20 10:20:30 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => (<h1>Hello, World from Docker React!</h1>)
+
+ReactDOM.render(<App />,  document.getElementById('app'))
